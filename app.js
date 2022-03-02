db3f0c06ef7d1e8c3a6b1ef9c832a7b5a1c335d2
Leonardo Vieira Sodré <LeonardoSodre25@outlook.com>
2022-03-02 19:51:13 -0300

    Projeto em andamento!

--- app.js
+++ app.js
@@ -28,3 +28,85 @@
       - Para obter a key e fazer requests, você terá que fazer login e escolher
         o plano free. Seus dados de cartão de crédito não serão solicitados.
 */
+
+const currencyOneEl = document.querySelector('[data-js="currency-one"]');
+const currencyTwoEl = document.querySelector('[data-js="currency-two"]');
+
+const currencysEl = document.querySelector('[data-js="currencies-container"]');
+
+const url = `https://v6.exchangerate-api.com/v6/8beaf1c7447dbd35bf3d0e0e/latest/USD`;
+
+const getErrorMessage = (errorType) =>
+  ({
+    "unsupported-code": "A moeda não existe em nosso banco de dados.",
+    "base-code-only-on-pro":
+      "Informações de moedas que não sejam USD ou EUR só podem ser acessadas a ",
+    "malformed-request":
+      "O endpoint do seu request precisa seguir a estrutura a seguir: ",
+    "invalid-key": "A chave da API não é válida",
+    "quota-reached":
+      "Sua conta alcançou o limite de requets permitido em seu plano atual",
+    "not-avaliable-on-plan":
+      "Seu plano atual não permite este tipo de request.",
+  }[errorType] || "Não foi possível obter as informações.");
+
+const fetchExchangeRate = async () => {
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(
+        "Sua conexão falhou.. Não foi possível obter as informações."
+      );
+    }
+    const exchangeRateData = await response.json();
+
+    if (exchangeRateData.result === "error") {
+      throw new Error(getErrorMessage(exchangeRateData["error-type"]));
+    }
+
+    return exchangeRateData;
+  } catch (err) {
+    const div = document.createElement("div");
+    const button = document.createElement("button");
+
+    div.textContent = err.message;
+    div.classList.add(
+      "alert",
+      "alert-warning",
+      "alert-dismissible",
+      "fade",
+      "show"
+    );
+    button.classList.add("btn-close");
+    div.setAttribute("role", "alert");
+    button.setAttribute("type", "button");
+    button.setAttribute("aria-label", "Close");
+
+    button.addEventListener("click", () => {
+      div.remove();
+    });
+
+    div.appendChild(button);
+    currencysEl.insertAdjacentElement("afterend", div);
+  }
+};
+
+const init = async () => {
+  const exchangeRateData = await fetchExchangeRate();
+
+  const getOptions = (selectedCurrency) =>
+    Object.keys(exchangeRateData.conversion_rates)
+      .map(
+        (currency) =>
+          `<option ${
+            currency === selectedCurrency ? "selected" : ""
+          }>${currency}</option>`
+      )
+      .join("");
+
+  currencyOneEl.innerHTML = getOptions("USD");
+  currencyTwoEl.innerHTML = getOptions("BRL");
+};
+
+init();
